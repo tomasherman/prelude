15d804d60b39330465eb500a8d85693fc094efa3
Tomas <tomas.herman@gmail.com>
2014-10-22 10:42:00 +0200

    added sunrise repo

---
+++ personal/repos.el
@@ -0,0 +1,3 @@
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                         ("gnu"  . "http://elpa.gnu.org/packages/")
+                         ("SC"   . "http://joseito.republika.pl/sunrise-commander/")))
