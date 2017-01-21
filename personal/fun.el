01aab70bf94585dce798f9e255d6cc7d56ccb730
Tomas Herman <hermant@avast.com>
2017-01-21 11:53:26 +0100

    Added chord snippet and stuff

--- personal/fun.el
+++ personal/fun.el
@@ -13,8 +13,14 @@
 (setq deft-extension "org")
 (setq deft-text-mode 'org-mode)
 
+
+(setq org-journal-dir "~/Dropbox/journal")
+(setq org-journal-file-format "%Y%m%d.org") 
+(setq org-journal-enable-encryption t)
 (setq org-log-done 'time)
+
 
 (add-hook 'org-mode-hook (lambda() (visual-line-mode 1)))
 (add-hook 'org-mode-hook (lambda() (whitespace-mode 0)))
 (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1)))
+(add-hook 'org-mode-hook (lambda() (yas-minor-mode 1)))
