fa3e01087fbbf0f390d862c7735ed1eb6f0ac4ca
Tomas Herman <hermant@avast.com>
2017-11-06 17:35:58 +0100

    custom

--- personal/custom.el
+++ personal/custom.el
@@ -6,11 +6,11 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(org-agenda-files (quote ("~/Dropbox/results/index.org")))
+ '(org-agenda-files (quote ("~/Dropbox/results/index.org")) t)
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (yaml-mode auctex org-trello neotree writeroom-mode darkroom fish-mode emojify org-dashboard org-journal org-bullets org-pomodoro puppet-mode sr-speedbar json-mode markdown-mode+ deft markdown-mode multiple-cursors geeknote haskell-mode key-chord anaconda-mode ensime auto-complete git-gutter+ vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+    (cmake-mode yaml-mode auctex org-trello neotree writeroom-mode darkroom fish-mode emojify org-dashboard org-journal org-bullets org-pomodoro puppet-mode sr-speedbar json-mode markdown-mode+ deft markdown-mode multiple-cursors geeknote haskell-mode key-chord anaconda-mode ensime auto-complete git-gutter+ vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
  '(prelude-clean-whitespace-on-save nil)
  '(prelude-global-mode t))
 (custom-set-faces
